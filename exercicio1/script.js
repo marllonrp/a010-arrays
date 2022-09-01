32b79de5512348db7abc975bc018776234286803
marllonrp <marllonrp@yahoo.com.br>
2022-09-01 12:03:06 -0300

    Atividades diárias

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numbers = [5, 11, 35, 145, 302, 521];
+
+const strings = ["ola", "bom dia", "boa tarde", "boa noite", "adeus", "tchau"];
+
+const mix = [12, "elemento", true, 87, "índice", false];
+
+console.log(numbers, strings, mix);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numbers = [5, 11, 35, 145, 302, 521];
+
+const strings = ["ola", "bom dia", "boa tarde", "boa noite", "adeus", "tchau"];
+
+const mix = [12, "elemento", true, 87, "índice", false];
+
+console.log(numbers.length);
+console.log(strings.length);
+console.log(mix.length);
+
+console.log(numbers[0]);
+console.log(strings[1]);
+console.log(mix[2]);
+
+console.log(numbers.includes(145));
+console.log(mix.includes("matrix"));
